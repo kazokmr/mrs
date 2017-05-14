c6aa7973cfff4d3eb53508b8ef43872435545f8b
kazokmr <engkokmr@gmail.com>
2017-05-15 00:17:29 +0900

    checkstyleの指摘対応

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -241,10 +241,14 @@ INSERT INTO reservable_room (reserved_date, room_id) VALUES (CURRENT_DATE - 1, 7
 
 -- ダミーユーザー(password=demo)
 INSERT INTO usr (user_id, first_name, last_name, role_name, password)
-VALUES ('taro-yamada', '太郎', '山田', 'USER', '$2a$10$oxSJl.keBwxmsMLkcT9lPeAIxfNTPNQxpeywMrF7A3kVszwUTqfTK');
+VALUES ('taro-yamada', '太郎', '山田', 'USER',
+        '$2a$10$oxSJl.keBwxmsMLkcT9lPeAIxfNTPNQxpeywMrF7A3kVszwUTqfTK');
 INSERT INTO usr (user_id, first_name, last_name, role_name, password)
-VALUES ('aaaa', 'Aaa', 'Aaa', 'USER', '$2a$10$oxSJl.keBwxmsMLkcT9lPeAIxfNTPNQxpeywMrF7A3kVszwUTqfTK');
+VALUES
+  ('aaaa', 'Aaa', 'Aaa', 'USER', '$2a$10$oxSJl.keBwxmsMLkcT9lPeAIxfNTPNQxpeywMrF7A3kVszwUTqfTK');
 INSERT INTO usr (user_id, first_name, last_name, role_name, password)
-VALUES ('bbbb', 'Bbb', 'Bbb', 'USER', '$2a$10$oxSJl.keBwxmsMLkcT9lPeAIxfNTPNQxpeywMrF7A3kVszwUTqfTK');
+VALUES
+  ('bbbb', 'Bbb', 'Bbb', 'USER', '$2a$10$oxSJl.keBwxmsMLkcT9lPeAIxfNTPNQxpeywMrF7A3kVszwUTqfTK');
 INSERT INTO usr (user_id, first_name, last_name, role_name, password)
-VALUES ('cccc', 'Ccc', 'Ccc', 'ADMIN', '$2a$10$oxSJl.keBwxmsMLkcT9lPeAIxfNTPNQxpeywMrF7A3kVszwUTqfTK');
+VALUES
+  ('cccc', 'Ccc', 'Ccc', 'ADMIN', '$2a$10$oxSJl.keBwxmsMLkcT9lPeAIxfNTPNQxpeywMrF7A3kVszwUTqfTK');
