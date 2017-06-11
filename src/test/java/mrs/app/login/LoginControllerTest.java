5af807ad54145f55080735928e5358df4e35f126
kazokmr <engkokmr@gmail.com>
2017-06-12 06:34:11 +0900

    LoginControllerのテストクラス作成

---
+++ src/test/java/mrs/app/login/LoginControllerTest.java
@@ -0,0 +1,30 @@
+package mrs.app.login;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+public class LoginControllerTest {
+  
+  MockMvc mockMvc;
+  
+  @Before
+  public void setUp() {
+    this.mockMvc = MockMvcBuilders.standaloneSetup(new LoginController())
+        .addFilters(new CharacterEncodingFilter("UTF-8"))
+        .build();
+  }
+  
+  @Test
+  public void loginformの呼び出しテスト() throws Exception {
+    mockMvc.perform(get("/loginForm"))
+        .andExpect(status().isOk())
+        .andExpect(forwardedUrl("login/loginForm"));
+  }
+}
